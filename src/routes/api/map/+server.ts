210ab5df1a4a87368d07a80d0933bca73de334ec
daniel eyassu <danieleyassu@hotmail.com>
2025-01-03 18:17:11 -0500

    Removed commenst

--- src/routes/api/map/+server.ts
+++ src/routes/api/map/+server.ts
@@ -4,9 +4,8 @@ import { GOOGLE_API_KEY } from '$env/static/private';
 const GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/js';
 
 export const GET: RequestHandler = async () => {
- console.log(GOOGLE_API_KEY)
     const url = `${GOOGLE_MAPS_API_URL}?key=${GOOGLE_API_KEY}&loading=async&libraries=places`;
-    console.log(url)
+
     const response = await fetch(url);
     const script = await response.text();
 
